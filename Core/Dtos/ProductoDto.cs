94c9b2a5a000978613dde85ffe088709c0a0c566
Onel Del Toro Rodríguez <onel.deltoro@gmail.com>
2021-10-13 12:06:03 -0400

    agregando Dtos

---
+++ Core/Dtos/ProductoDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace TodoApi.Core.Dtos
+{
+    public class ProductoDto
+    {
+        
+        public string Nombre { get; set; } 
+    }
+}
--- Core/Models/Factura.cs
+++ Core/Models/Factura.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace TodoApi.Models
 {
     public class Factura : BaseEntity
