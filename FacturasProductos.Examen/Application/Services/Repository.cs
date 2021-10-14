df0a1256eb06bb2971db64bbe8eff6b1c90c18f4
Onel Del Toro Rodríguez <onel.deltoro@gmail.com>
2021-10-14 05:37:04 -0400

    Repository

---
+++ FacturasProductos.Examen/Application/Services/Repository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FacturasProductos.Examen.Application.Infrastructure.Context;
+
+namespace FacturasProductos.Examen.Application.Services
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        protected readonly ApplicationDbContext ApplicationDbContext;
+
+        public Repository(ApplicationDbContext applicationDbContext)
+        {
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            try
+            {
+                return ApplicationDbContext.Set<TEntity>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retrieve entities: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(AddAsync)} entity must not be null");
+            }
+
+            try
+            {
+                await ApplicationDbContext.AddAsync(entity);
+                await ApplicationDbContext.SaveChangesAsync();
+
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be saved: {ex.Message}");
+            }
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(AddAsync)} entity must not be null");
+            }
+
+            try
+            {
+                ApplicationDbContext.Update(entity);
+                await ApplicationDbContext.SaveChangesAsync();
+
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be updated: {ex.Message}");
+            }
+        }
+    }
+}
